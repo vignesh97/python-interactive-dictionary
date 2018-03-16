01167abf8409ece27f000ab089cb8e8384c72046
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-16 18:48:25 -0400

    Importing difflib for find closer matches

--- find-meaning.py
+++ find-meaning.py
@@ -1,10 +1,14 @@
 import json
+#from difflib import SequenceMatcher
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word,data.keys())) > 0:
+        return "Did you mean %s instead ?" % get_close_matches(word, data.keys())[0]
     else:
         return "Word doesnt exist"
 
