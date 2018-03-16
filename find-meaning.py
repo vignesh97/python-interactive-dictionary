003b851c349bba92656f4cb7e001fc96314d373b
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-16 18:25:57 -0400

    IfElse check

--- find-meaning.py
+++ find-meaning.py
@@ -2,7 +2,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "Word doesnt exist"
 
 word = input("Enter word:")
 
